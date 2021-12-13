174351bdd7d52adec1b361e4d9b117e27aa19546
takeyuichi <y.takey@gmail.com>
2021-12-13 09:26:57 +0900

    support macOS 12

--- src/crop.ts
+++ src/crop.ts
@@ -20,8 +20,8 @@ const cropImage = async (filename) => {
   const leftWidth = metadata.width - rightWidth;
   const extractOPtions = { top: 0, height: metadata.height };
 
-  await image.extract({ ...extractOPtions, left: leftWidth, width: rightWidth }).toFile(`${outputPath}.a.${extension}`);
-  await image.extract({ ...extractOPtions, left: 0, width: leftWidth }).toFile(`${outputPath}.b.${extension}`);
+  await image.extract({ ...extractOPtions, left: leftWidth, width: rightWidth }).toFile(`${outputPath}.1.${extension}`);
+  await image.extract({ ...extractOPtions, left: 0, width: leftWidth }).toFile(`${outputPath}.2.${extension}`);
 };
 
 const main = async () => {
