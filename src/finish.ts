c8cd1d28d7e2d1aa81ee320fd26ed0aaca62695b
takeyuichi <y.takey@gmail.com>
2022-04-25 20:00:37 +0900

    add `finish` task

---
+++ src/finish.ts
@@ -0,0 +1,42 @@
+import fs from "fs";
+import path from "path";
+import sharp from "sharp";
+import yargs from 'yargs/yargs'
+import chalk from "chalk";
+
+import { imageDirPath, getImagePaths, cropImage } from "./io";
+
+const QUALITY = 50
+
+const argv = yargs(process.argv.slice(2)).options({ n: { type: "string", default: "01" } }).parseSync()
+const key = argv._[0] as string;
+const dir = imageDirPath(key)
+
+const crop = async () => {
+  const imagePaths = getImagePaths(dir);
+
+  for (let imagePath of imagePaths) {
+    await cropImage(imagePath)
+  }
+}
+
+const rename = async () => {
+  const imagePaths = getImagePaths(dir);
+  let counter = 0
+
+  for (let srcPath of imagePaths) {
+    const dstFile = path.join(dir, `${argv.n}_${String(++counter).padStart(5, '0')}.jpg`)
+    sharp(srcPath).jpeg({ quality: QUALITY }).toFile(dstFile).then(() => {
+      fs.unlink(srcPath, () => {})
+    });
+  }
+}
+
+const main = async () => {
+  await crop()
+  await rename()
+
+  console.log(chalk.green(`---- [${key}] Completed -----`));
+};
+
+main();
