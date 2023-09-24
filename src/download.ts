bebde20de1b5dee90d0e725e091ab400ed07d2e9
takeyuichi <y.takey@gmail.com>
2023-09-24 09:54:40 +0900

    support irregular file name

--- src/download.ts
+++ src/download.ts
@@ -6,13 +6,13 @@ import { readJSON, writeJSON, saveFile, getImageNum } from "./io";
 
 const [, , key] = process.argv;
 
-const sleep = (msec) => new Promise((resolve) => setTimeout(resolve, msec));
+const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
 
 const data = readJSON(key);
 
 (async () => {
   // console.log("---- [2] Start Downloading ---------------");
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
   // page.setViewport({ width: 1280, height: 926 });
 
@@ -26,28 +26,35 @@ const data = readJSON(key);
   for (const record of targets) {
     if (!record.done) {
       try {
-        const responseListener = async (response) => {
+        const responseListener = async response => {
           try {
-            const matches = /.+\/([^\/]{1,}\.(jpg|png))$/.exec(response.url());
-            if (!matches) return;
-            const filename = matches[1];
+            const getFilename = (url: string) => {
+              const funnyFileMatch = /.+\/([^\/]{1,}\-jpg)$/.exec(url);
+              if (funnyFileMatch) return funnyFileMatch[1] + ".jpg";
 
-            // タイトルと実際の拡張子が異なる場合があるため、拡張子は無視する
-            if (filename.replace(/\.(jpg|png)$/, "") !== record.filename.replace(/\.(jpg|png)$/, "")) return;
+              const matches = /.+\/([^\/]{1,}\.(jpg|png))$/.exec(response.url());
+              if (!matches) return null;
+              const filename = matches[1];
+
+              // タイトルと実際の拡張子が異なる場合があるため、拡張子は無視する
+              if (filename.replace(/\.(jpg|png)$/, "") !== record.filename.replace(/\.(jpg|png)$/, "")) return null;
+              return filename;
+            };
+            const filename = getFilename(response.url());
+            if (!filename) return;
 
             const buffer = await response.buffer();
             await saveFile(key, `${record.page.toString().padStart(3, "0")}_${filename}`, buffer);
-              
           } catch (e) {}
         };
-        page.on("response", responseListener)
-      
+        page.on("response", responseListener);
+
         // `{ waitUntil: "networkidle0" }` だと画像の保存が完了する前に終了することがあるため、
         // 完全にアイドル状態になるまで待機する。もしそれでも取りこぼしが発生する場合は、
         // "domcontentloaded" や "load" を試してみる。
         const response = await page.goto(record.url, { timeout: 20000, waitUntil: "networkidle0" });
         await sleep(1000);
-        page.off("response", responseListener)
+        page.off("response", responseListener);
 
         if (response.status() === 200) {
           record.done = true;
@@ -68,7 +75,7 @@ const data = readJSON(key);
     failures.forEach(({ page, url }) => {
       console.log(`${page}: ${url}`);
     });
-    return
+    return;
   }
 
   if (getImageNum(key) === data.size) {
