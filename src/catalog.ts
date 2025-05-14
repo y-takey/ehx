c9ecffecf8d39e8df70e00775028e69004c0be4b
takeyuichi <y.takey@gmail.com>
2025-05-14 14:33:56 +0900

    adjust timeout seconds for index page

--- src/catalog.ts
+++ src/catalog.ts
@@ -81,7 +81,7 @@ const uniq = (ary: string[]) => Array.from(new Set(ary));
     const urls = [];
     for (const indexPage of pagenations) {
       if (indexPage !== targetUrl) {
-        await page.goto(indexPage, gotoOptions);
+        await page.goto(indexPage, { timeout: 20000, ...gotoOptions });
       }
 
       urls.push(...(await getHrefs(page, ThumbnailSelector)));
