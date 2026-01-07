339675f4447acee9c8f2afc9ef0561d424f3cd05
takeyuichi <y.takey@gmail.com>
2026-01-07 15:28:49 +0900

    upgrade dependency packages

--- src/puppeteer.ts
+++ src/puppeteer.ts
@@ -2,6 +2,4 @@ import puppeteer, { Browser, WaitForOptions } from "puppeteer";
 
 export const gotoOptions: WaitForOptions = { waitUntil: "domcontentloaded" };
 
-export const launch = async (): Promise<Browser> =>
-  // --headless=old : hide bounded icon on the dock
-  puppeteer.launch({ headless: true, args: ["--headless=old"] });
+export const launch = async (): Promise<Browser> => puppeteer.launch({ headless: true });
